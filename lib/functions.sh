fee03adece79ee3690477b90b9460d4183037930
Rasso Hilber <mail@rassohilber.com>
2024-01-09 21:17:46 +0100

    Make script more modular with functions

---
+++ lib/functions.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+# Log a string
+function log() {
+    printf "\r\n$1";
+}
+
+# Log an empty line
+function logLine() {
+    log ""
+}
+
+# Log an error message and exit with code 1
+function logError() {
+    log "🚨${BOLD}${RED} Error: ${NC}$1";
+    exit 1;
+}
+
+# Log a success message
+function logSuccess() {
+    log "✅${BOLD}${GREEN} Success: ${NC}$1";
+}
+
+# Check the git branch from the theme
+function checkProductionBranch() {
+    # Bail early if not deploying to production
+    [[ $REMOTE_ENV != "production" ]] && return;
+
+    # Get the branch from the theme
+    cd "$ROOT_DIR/content/themes/$WP_THEME";
+    BRANCH=$(git branch --show);
+    cd $ROOT_DIR;
+
+    # Check it
+    if [[ ! $BRANCH =~ $MAIN_BRANCH ]]; then
+        logError "Deploying to production is only allowed from ${NC}\$MAIN_BRANCH${RED} ($MAIN_BRANCH)"
+    else
+        logSuccess "Branch $BRANCH allowed in production"
+    fi
+}
+
+# Check if there is a file `.allow-deployment` present at the remote root
+function checkIsRemoteAllowed() {
+    ALLOWED=$(eval "ssh $SSH_USER@$SSH_HOST 'cd $SSH_PATH; [ -f \"$SSH_PATH/.allow-deployment\" ] && echo 1'")
+
+    if [[ $ALLOWED != 1 ]]; then
+        log "" "${RED}$SSH_USER@$SSH_HOST:$SSH_PATH${NC}"
+        logError "Remote root not allowed for deployment (missing file .allow-deployment)"
+    else
+        logSuccess ".allow-deployment detected on remote server"
+    fi;
+}
+
+# Check the PHP version between two environments
+function checkPHPVersions() {
+    # The file name for the PHP check. Appends a hash to make the file hard to detect.
+    # The file will be automatically deleted after this script finishes
+    local HASH=$(echo $(date +%s%N) | sha256sum | head -c 10)
+    local FILE_NAME="___wp-sync-deploy-php-version-$HASH.php"
+
+    # Get the local PHP Version
+    echo "<?= phpversion();" > "./$FILE_NAME"
+    LOCAL_VERSION=$(curl -s "https://$DEV_URL/$FILE_NAME")
+    # substring from position 0-3
+    LOCAL_VERSION=${LOCAL_VERSION:0:3}
+    rm "./$FILE_NAME"
+    log "- PHP version ${GREEN}$LOCAL_VERSION${NC} detected at ${BOLD}$DEV_URL${NC}"
+
+    # Write a file to the web root that prints the php version
+    ssh "$SSH_USER@$SSH_HOST" "cd $SSH_PATH; echo '<?= substr(phpversion(), 0, 3);' > ./$FILE_NAME"
+
+    # Get the version using CURL
+    REMOTE_FILE_URL="https://$REMOTE_URL/$FILE_NAME"
+
+    # Check if the remote file actually exists
+    REMOTE_FILE_RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" $REMOTE_FILE_URL)
+    [[ $REMOTE_FILE_RESPONSE_CODE != 200 ]] && logError "Something went wrong while trying to detect the remote PHP version. Please try again."
+
+    REMOTE_VERSION=$(curl -s $REMOTE_FILE_URL)
+    # substring from position 0-3
+    REMOTE_VERSION=${REMOTE_VERSION:0:3}
+    log "- PHP version ${GREEN}$REMOTE_VERSION${NC} detected at ${BOLD}$REMOTE_URL${NC}"
+
+    # Remove the file to prevent security issues
+    ssh "$SSH_USER@$SSH_HOST" "cd $SSH_PATH; rm ./$FILE_NAME"
+
+    # Error out if the two PHP versions aren't a match
+    if [[ "$LOCAL_VERSION" != "$REMOTE_VERSION" ]]; then
+        logError "PHP version mismatch, aborting"
+    else
+        logSuccess "PHP versions match between environments"
+    fi
+}
+
+# Checks if a file exists on a remote server
+function checkRemoteFile() {
+    ssh $SSH_USER@$SSH_HOST "[ -e \"$SSH_PATH/$1\" ] && echo 1";
+}
+
+# Validate that the required directories exist locally and remotely
+function checkDirectories() {
+    for deploy_dir in $DEPLOY_DIRS; do
+        # check on remote machine
+        if [[ $(checkRemoteFile $deploy_dir) != 1 ]]; then
+            logError "The directory ${GREEN}$deploy_dir${RED} does not exist on the remote server"
+        fi
+        # check on local machine
+        if [ ! -d "$ROOT_DIR/$deploy_dir" ]; then
+            logError "The directory ${GREEN}$ROOT_DIR/$deploy_dir${RED} does not exist locally"
+        fi
+    done
+
+    logSuccess "All directories exist! Proceeding..."
+}
--- wp-sync-deploy.sh
+++ wp-sync-deploy.sh
@@ -20,6 +20,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# The directory relative to the script
+export ROOT_DIR=$(pwd)
+export SCRIPT_DIR=$(realpath $(dirname $0))
+
 # Font Colors
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -30,19 +34,12 @@ export NC='\033[0m' # No Color
 export BOLD=$(tput bold)
 export NORMAL=$(tput sgr0)
 
+# Source files
+source "$SCRIPT_DIR/lib/functions.sh"
+
 # Required positional arguments
 JOB_NAME="$1"
 REMOTE_ENV="$2"
-
-# Print a message and exit with code 1
-errorOut() {
-    printf "\n🚨${BOLD}${RED} $1";
-    exit 1;
-}
-
-# The directory relative to the script
-DIR=$(pwd)
-SCRIPT_DIR=$(realpath $(dirname $0))
 
 # Deployment to production will only be possible from these two branches
 MAIN_BRANCH="master|main"
@@ -61,7 +58,7 @@ findUp() {
 
 # Find the closest .env file
 ENV_FILE=$(findUp ".env" $SCRIPT_DIR)
-[ -z "$ENV_FILE" ] && errorOut "No matching .env file found"
+[ -z "$ENV_FILE" ] && logError "No matching .env file found"
 
 # Make env vars available everywhere
 # @see https://stackoverflow.com/a/30969768/586823
@@ -70,21 +67,14 @@ source $ENV_FILE
 set +o allexport
 
 # Validate the dir this script should be called from
-if [[ "$DIR" == "$SCRIPT_DIR" ]]; then
-    errorOut "This script should always be called from the parent directory"
+if [[ "$ROOT_DIR" == "$SCRIPT_DIR" ]]; then
+    logError "This script should always be called from the parent directory"
 fi;
-
-# Echo the git branch from the theme
-getThemeBranch() {
-    cd "$DIR/content/themes/$WP_THEME";
-    git branch --show;
-    cd $DIR;
-}
 
 # Exit early if we received no arguments
 if [ $# -eq 0 ]
 then
-    errorOut "No arguments provided, exiting..."
+    logError "No arguments provided, exiting..."
 fi
 
 # Set SSH paths based on provided environment (production/staging)
@@ -107,7 +97,7 @@ case $REMOTE_ENV in
     ;;
 
     *)
-        errorOut "Please provide the remote environment (production or staging)"
+        logError "Please provide the remote environment (production or staging)"
     ;;
 esac
 
@@ -115,27 +105,6 @@ export REMOTE_CACHE_PATH="$SSH_PATH$PROD_CACHE_PATH"
 
 ERROR_MESSAGE="That did not work. Please check your command and try again"
 
-# Checks if a file exists on a remote server
-checkRemoteFile() {
-    ssh $SSH_USER@$SSH_HOST "[ -e \"$SSH_PATH/$1\" ] && echo 1";
-}
-
-# Validate that the required directories exist locally and remotely
-for deploy_dir in $DEPLOY_DIRS; do
-    # check on remote machine
-    if [[ $(checkRemoteFile $deploy_dir) != 1 ]]; then
-        errorOut "The directory ${GREEN}$deploy_dir${RED} does not exist on the remote server"
-    fi
-    # check on local machine
-    if [ ! -d "$DIR/$deploy_dir" ]; then
-        errorOut "The directory ${GREEN}$DIR/$deploy_dir${RED} does not exist locally"
-    fi
-done
-
-
-isRemoteRootAllowed() {
-    eval "ssh $SSH_USER@$SSH_HOST 'cd $SSH_PATH; [ -f \"$SSH_PATH/.allow-deployment\" ] && echo yes'"
-}
 
 case $JOB_NAME in
 
@@ -156,22 +125,22 @@ case $JOB_NAME in
         REMOTE_FILE="remote-$REMOTE_DB_NAME.sql"
         LOCAL_FILE="local-$LOCAL_DB_NAME.sql"
 
-        printf "\r\n💾 Dumping remote database to $REMOTE_FILE\n"
+        log "💾 Dumping remote database to $REMOTE_FILE\n"
         eval "ssh $SSH_USER@$SSH_HOST 'mysqldump -h $REMOTE_DB_HOST -u$REMOTE_DB_USER -p$REMOTE_DB_PASS $REMOTE_DB_NAME --default-character-set=utf8mb4' > '$SCRIPT_DIR/$REMOTE_FILE'"
 
-        printf "\r\n💾 Dumping local database to $LOCAL_FILE\n"
+        log "💾 Dumping local database to $LOCAL_FILE\n"
         eval "mysqldump -h $LOCAL_DB_HOST -u$LOCAL_DB_USER -p$LOCAL_DB_PASS $LOCAL_DB_NAME > '$SCRIPT_DIR/$LOCAL_FILE'"
 
-        printf "\r\n⬇️ Importing remote database into local database"
+        log "⬇️ Importing remote database into local database"
         eval "mysql -h $LOCAL_DB_HOST -u$LOCAL_DB_USER -p$LOCAL_DB_PASS $LOCAL_DB_NAME < '$SCRIPT_DIR/$REMOTE_FILE'"
 
         rm "$SCRIPT_DIR/$REMOTE_FILE";
         rm "$SCRIPT_DIR/$LOCAL_FILE";
 
-        printf "\r\n🔄 Replacing $REMOTE_URL with $DEV_URL..."
+        log "🔄 Replacing $REMOTE_URL with $DEV_URL..."
         wp search-replace "$REMOTE_URL" "$DEV_URL" --all-tables-with-prefix --skip-plugins="qtranslate-xt"
 
-        printf "\n🔄 Syncing ACF field groups..."
+        log "\n🔄 Syncing ACF field groups..."
         # @see https://gist.github.com/hirasso/c48c04def92f839f6264349a1be773b3
         # If you don't need this, go ahead and comment it out
         wp rhau acf-sync-field-groups --skip-plugins="qtranslate-xt"
@@ -179,25 +148,21 @@ case $JOB_NAME in
         # Deactivate maintenance mode
         wp maintenance-mode deactivate --skip-plugins="qtranslate-xt"
 
-        printf "\n\n✅ Done!"
+        log "\n✅ Done!"
     ;;
 
     # DEPLOY to production or staging server
     deploy)
-        # Allow deployment to production only from $MAIN_BRANCH
-        if [[ $REMOTE_ENV == "production" && ! $(getThemeBranch) =~ $MAIN_BRANCH ]]; then
-            errorOut "Deploying to production is only allowed from ${NC}\$MAIN_BRANCH${RED} ($MAIN_BRANCH)"
-        fi
+        logLine
+        checkDirectories
 
-        # Validate if the remote root is writable
-        if [[ $(isRemoteRootAllowed) != 'yes' ]]; then
-            errorOut "Remote root not allowed for deployment (missing file .allow-deployment): ${GREEN}$SSH_PATH";
-        fi;
+        checkPHPVersions
 
-        # Check for a matching PHP version between the local and remote environment
-        chmod +x "$SCRIPT_DIR/check-php.sh"
-        "$SCRIPT_DIR/check-php.sh"
-        exit;
+        checkProductionBranch
+
+        checkIsRemoteAllowed
+
+        logLine
 
         DEPLOY_MODE="dry"
         if [[ ! -z "${3+x}" && $3 == 'run' ]]; then
@@ -206,31 +171,29 @@ case $JOB_NAME in
 
         case $DEPLOY_MODE in
 
-            # ./deployment/deploy.sh deploy production
             dry)
-                printf "\r\n🚀 ${GREEN}${BOLD}[ DRY-RUN ]${NORMAL}${NC} Deploying to production\r\n"
+                log "🚀 ${GREEN}${BOLD}[ DRY-RUN ]${NORMAL}${NC} Deploying to production\r\n"
                 rsync --dry-run -az --delete --progress --relative --exclude-from "$SCRIPT_DIR/.deployignore" $DEPLOY_DIRS "$SSH_USER@$SSH_HOST:$SSH_PATH" | tee "$SCRIPT_DIR/.deploy-$REMOTE_ENV.log"
             ;;
 
-            # ./deployment/deploy.sh deploy production run
             run)
-                # Build assets in your theme. Deactivate/modify this if you don't have a npm script called "build"
-                cd "$DIR/content/themes/$WP_THEME";
+                # Build assets in the provided theme. Deactivate/modify this if you don't have an npm script called "build"
+                cd "$ROOT_DIR/content/themes/$WP_THEME";
                 npm run build
-                cd $DIR;
+                cd $ROOT_DIR;
 
                 # Deploy
-                printf "\r\n🚀 ${GREEN}${BOLD}[ LIVE ]${NORMAL}${NC} Deploying to production…"
+                log "🚀 ${GREEN}${BOLD}[ LIVE ]${NORMAL}${NC} Deploying to production…"
                 rsync -avz --delete --relative --exclude-from "$SCRIPT_DIR/.deployignore" $DEPLOY_DIRS "$SSH_USER@$SSH_HOST:$SSH_PATH" | tee "$SCRIPT_DIR/.deploy-$REMOTE_ENV.log"
 
                 # Clear the cache folder
-                printf "\r\n${BOLD}Clearing the cache cache at:${NORMAL}\r\n $REMOTE_CACHE_PATH"
+                log "${BOLD}Clearing the cache cache at:${NORMAL}\r\n $REMOTE_CACHE_PATH"
                 ssh $SSH_USER@$SSH_HOST "rm -r $REMOTE_CACHE_PATH"
                 exit 0;
             ;;
 
             *)
-                errorOut $ERROR_MESSAGE
+                logError $ERROR_MESSAGE
             ;;
 
         esac
@@ -238,7 +201,7 @@ case $JOB_NAME in
 
     # Nothing matched, print an error
     *)
-        errorOut "$ERROR_MESSAGE"
+        logError "$ERROR_MESSAGE"
     ;;
 
 esac
